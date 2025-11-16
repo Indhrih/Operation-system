59358fef995b3d2fa03a9264cf18486464b24800
Indhrih <sasa-rozhkov@yandex.ru>
2025-11-16 16:38:09 +0300

    Update commands and add process management tasks

--- Practice 5.bash
+++ Practice 5.bash
@@ -9,11 +9,32 @@ jobs
 mc &
 ps -fl -p $(prgrep mc)
 #4 Задание
+w
 #5 Задание
+ps
+ps -f
 #6 Задание
+ps -ef --forest
 #7 Задание
+pstree
 #8 Задание
+kill -TERM <PPID>
+kill -INT <PPID>
+kill -QUIT <PPID>
+kill -HUP <PPID>
 #9 Задание
+sleep 1000 &
+kill -TERM %1
+kill -INT %1
+kill -QUIT %1
+kill -HUP %1
 #10 Задание
+trap "touch myfile" TERM
+# проверяем
+kill -TERM $$
+ls myfile
 #11 Задание
+nice -n -1 bash
 #12 Задание
+sudo timme nice -n 19 updatedb
+sudo timme nice -n 5 updatedb
